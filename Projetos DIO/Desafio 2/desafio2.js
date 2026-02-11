d53c43cb4c9d2bb01c389883f23f304e69f9c385
Joana-Pacheco <pacheco.jo86@gmail.com>
2026-02-11 17:57:00 -0300

    desafio2

---
+++ Projetos DIO/Desafio 2/desafio2.js
@@ -0,0 +1,24 @@
+// definir a função 
+function ClassificarHeroi (vitorias,derrotas) {
+// retornar o resultado 
+  return vitorias - derrotas; 
+  let saldoFinal =  "vitorias - derrotas"
+  let nivel
+}
+
+//estrutura de repetição
+if (vitorias <=10) {
+    nivel = "Ferro" ;
+} if else (vitorias <=20) {
+    nivel = "Bronze";
+} if else (vitorias <=50)
+    nível = "Prata"; {
+} if else (vitorias <=80)
+    nível = "Ouro"; {
+{ if else (vitorias <=90)
+    nível = "diamante"; {
+{if else (vitorias <=100) 
+    nível = Lendário"; {
+{else  {
+    nível "imortal"
+}   
